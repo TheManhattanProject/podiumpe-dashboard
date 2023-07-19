ff095709104ba52310a7609aa44b1ff98b6e8c16
om agarwal <omag.about@gmail.com>
2023-07-20 00:11:25 +0530

    ci: husky/lint/debugger

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,21 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
+
+export default function Dashboard() {
+  const [first, setfirst] = useState('dcsd');
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Input
+        type="email"
+        placeholder="Email"
+        state={first}
+        setState={setfirst}
+      />
+      <Button variant="secondary">Button</Button>
+
+      <pre>{first}</pre>
+    </main>
+  );
+}
